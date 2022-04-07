4238da876459a5764285aab7561f12f223f0c2f9
Sfzmango <maunghtike1999@gmail.com>
2022-04-06 19:44:33 -0700

    WIP for moment .isBefore code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,14 @@
+var currentHour = moment().hour();
 
-
-
-
-
+console.log(currentHour);
 
 var today = moment().format("dddd, MMM Do");
 $("#currentDay").text(today);
+
+function check9am() {
+    if (currentHour.isBefore("9")) {
+        $("#9am").addClass(".past")
+    }
+};
+
+check9am();
